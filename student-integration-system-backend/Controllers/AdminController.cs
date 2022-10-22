ff08a215104da971dccacf1d99a097e2ee091247
Miroslaw Karwowski <mirek5207@gmail.com>
2022-10-22 18:23:43 +0200

    feat: add authorization to api/Admin/createModeratorAccount endpoint, only admin can create moderator

--- student-integration-system-backend/Controllers/AdminController.cs
+++ student-integration-system-backend/Controllers/AdminController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using student_integration_system_backend.Entities;
 using student_integration_system_backend.Models.Request;
 using student_integration_system_backend.Models.Response;
+using student_integration_system_backend.Models.Seeds;
 using student_integration_system_backend.Services.ModeratorService;
 
 namespace student_integration_system_backend.Controllers;
@@ -21,6 +24,7 @@ public class AdminController : ControllerBase
     /// Creates new moderator
     /// </summary>
     [HttpPost("createModeratorAccount")]
+    [Authorize(Roles = RoleType.Admin, AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public ActionResult<AuthenticationResponse> CreateModerator(ModeratorSignUpRequest request)
     {
         var response = _moderatorService.RegisterModerator(request);
