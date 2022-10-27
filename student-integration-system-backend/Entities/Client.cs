1fff8067d33aa0153d7e65d1a3b865d0b8ea1f34
Miroslaw Karwowski <mirek5207@gmail.com>
2022-10-27 23:29:18 +0200

    fix: foreign keys in Friend entity

--- student-integration-system-backend/Entities/Client.cs
+++ student-integration-system-backend/Entities/Client.cs
@@ -1,4 +1,6 @@
-﻿namespace student_integration_system_backend.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace student_integration_system_backend.Entities;
 
 public class Client
 {
@@ -6,8 +8,7 @@ public class Client
     public string FirstName { get; set; }
     public string SurName { get; set; }
     public int UserId { get; set; }
-    public virtual User User { get; set; }
-    public virtual List<Friend> Friends { get; set; }
+    [ForeignKey("UserId")] public virtual User User { get; set; }
     public virtual List<CustomPlace> CustomPlaces { get; set; }
     public virtual List<LobbyOwner> LobbyOwners { get; set; }
     public virtual List<LobbyGuest> LobbyGuests { get; set; }
--- student-integration-system-backend/Entities/Friend.cs
+++ student-integration-system-backend/Entities/Friend.cs
@@ -1,7 +1,18 @@
-﻿namespace student_integration_system_backend.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace student_integration_system_backend.Entities;
 
 public class Friend
 {
     public int Id { get; set; }
-    public virtual List<Friend> Friends { get; set; }
+    public int FriendId { get; set; }
+    public int UserId { get; set; }
+    
+    [ForeignKey("FriendId")]
+    public virtual User FriendUser { get; set; }
+    [ForeignKey("UserId")]
+    public virtual User User { get; set; }
+    
+    
+
 }
