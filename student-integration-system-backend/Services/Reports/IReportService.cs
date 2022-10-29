0eb7568e4b26b4b77d5ad3f5a63d32c9702bfca4
Miroslaw Karwowski <mirek5207@gmail.com>
2022-10-29 11:15:16 +0200

    feat: add get all system reports and update status of report endpoint

---
+++ student-integration-system-backend/Services/Reports/IReportService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using student_integration_system_backend.Entities;
+
+namespace student_integration_system_backend.Services.Reports;
+
+public interface IReportService
+{
+    IEnumerable<Report> GetAllReportsByReportType(ReportType reportType);
+    Report UpdateStatusOfReport(int reportId, ReportStatus reportStatus);
+}
---
+++ student-integration-system-backend/Services/Reports/ReportServiceImpl.cs
@@ -0,0 +1,36 @@
+﻿using student_integration_system_backend.Entities;
+using student_integration_system_backend.Exceptions;
+
+namespace student_integration_system_backend.Services.Reports;
+
+public class ReportServiceImpl : IReportService
+{
+    private readonly AppDbContext _dbContext;
+
+    public ReportServiceImpl(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public IEnumerable<Report> GetAllReportsByReportType(ReportType reportType)
+    {
+        var reports = _dbContext.Reports.ToList().Where(report => report.ReportType == reportType );
+        if (reports == null) throw new NotFoundException("Not found any reports");
+        return reports;
+    }
+
+    public Report UpdateStatusOfReport(int reportId, ReportStatus reportStatus)
+    {
+        var report = GetReportById(reportId);
+        report.Status = reportStatus;
+        _dbContext.SaveChanges();
+        return report;
+    }
+
+    private Report GetReportById(int reportId)
+    {
+        var report = _dbContext.Reports.FirstOrDefault(report => report.Id == reportId);
+        if (report == null) throw new NotFoundException("Report not found");
+        return report;
+    }
+}
