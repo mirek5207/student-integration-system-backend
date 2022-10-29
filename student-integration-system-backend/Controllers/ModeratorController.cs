a5c2e0416ad151b7e1d58d0a9dced9d02ca4fef9
Miroslaw Karwowski <mirek5207@gmail.com>
2022-10-29 13:29:39 +0200

    feat: implemented get all user reports endpoint

---
+++ student-integration-system-backend/Controllers/ModeratorController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using student_integration_system_backend.Entities;
+using student_integration_system_backend.Models.Seeds;
+using student_integration_system_backend.Services.Reports;
+
+namespace student_integration_system_backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ModeratorController : ControllerBase
+{
+    private readonly IReportService _reportService;
+
+    public ModeratorController(IReportService reportService)
+    {
+        _reportService = reportService;
+    }
+    
+    /// <summary>
+    /// Get all user reports
+    /// </summary>
+    [HttpGet("getUserReports")]
+    [Authorize(Roles = RoleType.Moderator, AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public ActionResult<List<Report>> GetAllUserReports()
+    {
+        var response = _reportService.GetAllReportsByReportType(ReportType.UserReport);
+        return Ok(response);
+    }
+    
+}
