1ecbad21588fac50ef4bf611a6cb333a3fba4cf3
Miroslaw Karwowski <mirek5207@gmail.com>
2022-11-06 11:10:38 +0100

    fix: change data type for latitude and longitude from string to double in Place object

--- student-integration-system-backend/Entities/Place.cs
+++ student-integration-system-backend/Entities/Place.cs
@@ -6,8 +6,8 @@ public class Place
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Latitude { get; set; }
-    public string Longitude { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
     public int PlaceOwnerId { get; set; }
     public virtual PlaceOwner PlaceOwner { get; set; }
 }
