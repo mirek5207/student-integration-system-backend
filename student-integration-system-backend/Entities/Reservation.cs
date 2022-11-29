26bcf0ebf704f0872f83b187af216237cf0d3a57
Maciej Klimiuk <maciek120200@gmail.com>
2022-11-29 16:59:45 +0100

    feat: add reservation entity

---
+++ student-integration-system-backend/Entities/Reservation.cs
@@ -0,0 +1,21 @@
+﻿namespace student_integration_system_backend.Entities;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int NumberOfGuests { get; set; }
+    public ReservationStatus Status { get; set; }
+    public int PlaceId { get; set; }
+    public int LobbyId { get; set; }
+    public virtual Place Place { get; set; }
+    public virtual Lobby Lobby { get; set; }
+}
+
+public enum ReservationStatus
+{
+    Sent,
+    Confirmed,
+    Declined
+}
