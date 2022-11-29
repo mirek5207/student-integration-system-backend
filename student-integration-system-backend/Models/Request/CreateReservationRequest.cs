f95a13c615ad5f9dae52b3531e7a34d83351abcb
Maciej Klimiuk <maciek120200@gmail.com>
2022-11-29 17:37:48 +0100

    feat: add request model to create reservation

---
+++ student-integration-system-backend/Models/Request/CreateReservationRequest.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using student_integration_system_backend.Entities;
+
+namespace student_integration_system_backend.Models.Request;
+
+public class CreateReservationRequest
+{
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int NumberOfGuests { get; set; }
+    public int PlaceId { get; set; }
+    public int LobbyId { get; set; }
+}
+
+public class CreateReservationRequestValidator : AbstractValidator<CreateReservationRequest>
+{
+    public CreateReservationRequestValidator()
+    {
+        RuleFor(r => r.StartDate)
+            .NotNull().WithMessage("Start date is required");
+        RuleFor(r => r.EndDate)
+            .NotNull().WithMessage("End date is required");
+        RuleFor(r => r.NumberOfGuests)
+            .GreaterThan(0).WithMessage("Number of reserved seats must be greater than 0.");
+        RuleFor(r => r.PlaceId)
+            .NotEmpty().WithMessage("PlaceId is required");
+        RuleFor(r => r.LobbyId)
+            .NotEmpty().WithMessage("LobbyId is required");
+    }
+}
