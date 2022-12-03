31c29cbb9d80ac3eb5f348b076fa3325bbbaf906
Maciej Klimiuk <maciek120200@gmail.com>
2022-12-03 13:26:44 +0100

    feat: add requests to create lobby with place or custom place

---
+++ student-integration-system-backend/Models/Request/CreateLobbyAtCustomPlaceRequest.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using student_integration_system_backend.Entities;
+
+namespace student_integration_system_backend.Models.Request;
+
+public class CreateLobbyAtCustomPlaceRequest
+{
+    public int MaxSeats { get; set; }
+    public string Name { get; set; }
+    public DateTime StartDate { get; set; }
+    public LobbyType Type { get; set; }
+    public int? CustomPlaceId { get; set; }
+    public string CustomPlaceName { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string? Description { get; set; }
+}
+
+public class CreateLobbyAtCustomPlaceRequestValidator : AbstractValidator<CreateLobbyAtCustomPlaceRequest>
+{
+    public CreateLobbyAtCustomPlaceRequestValidator()
+    {
+        RuleFor(l => l.MaxSeats)
+            .GreaterThan(0).WithMessage("MaxSeats have to be greater than 0");
+        RuleFor(l => l.Name)
+            .NotNull().WithMessage("Name is required");
+        RuleFor(l => l.Type)
+            .IsInEnum().WithMessage("Lobby type have to be in enum range");
+        RuleFor(l => l.StartDate)
+            .NotNull().WithMessage("Start date is required");
+        When(l => l.CustomPlaceId != null, () =>
+        {
+            RuleFor(l => l.CustomPlaceId)
+                .GreaterThan(0).WithMessage("CustomPlaceId have to be greater than 0");
+        });
+        RuleFor(l => l.CustomPlaceName)
+            .NotNull().WithMessage("Custom place name is required");
+        RuleFor(l => l.Latitude)
+            .NotNull().WithMessage("Latitude is required");
+        RuleFor(l => l.Longitude)
+            .NotNull().WithMessage("Longitude is required");
+    }
+}
---
+++ student-integration-system-backend/Models/Request/CreateLobbyAtPlaceRequest.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using student_integration_system_backend.Entities;
+
+namespace student_integration_system_backend.Models.Request;
+
+public class CreateLobbyAtPlaceRequest
+{
+    public int MaxSeats { get; set; }
+    public string Name { get; set; }
+    public DateTime StartDate { get; set; }
+    public LobbyType Type { get; set; }
+    public int PlaceId { get; set; }
+}
+
+public class CreateLobbyAtPlaceRequestValidator : AbstractValidator<CreateLobbyAtPlaceRequest>
+{
+    public CreateLobbyAtPlaceRequestValidator()
+    {
+        RuleFor(l => l.MaxSeats)
+            .GreaterThan(0).WithMessage("MaxSeats have to be greater than 0");
+        RuleFor(l => l.Name)
+            .NotNull().WithMessage("Name is required");
+        RuleFor(l => l.Type)
+            .IsInEnum().WithMessage("Lobby type have to be in enum range");
+        RuleFor(l => l.StartDate)
+            .NotNull().WithMessage("Start date is required");
+        RuleFor(l => l.PlaceId)
+            .NotNull().WithMessage("Place id is required");
+    }
+}
